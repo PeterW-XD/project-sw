178d2190fb8c3059262ed407e5c991f0b0af3532
PeterW-XD <peiran_wang@outlook.com>
2024-03-28 18:36:37 -0400

    initial

---
+++ hello.c
@@ -0,0 +1,107 @@
+/*
+ * Userspace program that communicates with the vga_ball device driver
+ * through ioctls
+ *
+ * Stephen A. Edwards
+ * Columbia University
+ */
+
+#include <stdio.h>
+#include "vga_ball.h"
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include "write_wav.h"
+
+// int vga_ball_fd;
+int project_top_fd;
+const int sample_rate = 48000;	// 48000kHz
+const int word_length = 24;			// 24 bits per sample
+const int duration_sec = 10;		// 10s
+const int buf_size = sample_rate * duration_sec;
+int left_buf[buf_size];
+int right_buf[buf_size];
+int index = 0;
+
+/* Read and print the background color */
+//void print_background_color() {
+void read_audio() {
+  vga_ball_arg_t vla;
+  
+  if (ioctl(project_top_fd, AUDIO_READ, &vla)) {
+      perror("ioctl(READ) failed");
+      return;
+  }
+	if (vla.audio.ready == 1) {
+		left_buf[index++] = vla.audio.left;
+		right_buf[index++] = vla.audio.right;
+	}
+  //printf("%02x %02x %02x\n",
+	// vla.background.red, vla.background.green, vla.background.blue);
+}
+
+/* Set the background color */
+//void set_background_color(const vga_ball_color_t *c)
+//{
+//  vga_ball_arg_t vla;
+//  vla.background = *c;
+//  if (ioctl(vga_ball_fd, VGA_BALL_WRITE_BACKGROUND, &vla)) {
+//      perror("ioctl(VGA_BALL_SET_BACKGROUND) failed");
+//      return;
+//  }
+//}
+
+int main()
+{
+  // int i;
+  // static const char filename[] = "/dev/vga_ball";
+  static const char filename[] = "/dev/project_top";
+  static const char file1[] = "test1.wav";
+  static const char file2[] = "test2.wav";
+  printf("Audio record program started\n");
+  if ( (project_top_fd = open(filename, O_RDWR)) == -1) {
+    fprintf(stderr, "could not open %s\n", filename);
+    return -1;
+  }
+	while (index < buf_size) {
+		read_audio();
+	}
+	write_wav(file1, sample_rate, word_length, duration_sec, left_buf);
+	write_wav(file2, sample_rate, word_length, duration_sec, right_buf);
+
+//  static const vga_ball_color_t colors[] = {
+//    { 0xff, 0x00, 0x00 }, /* Red */
+//    { 0x00, 0xff, 0x00 }, /* Green */
+//    { 0x00, 0x00, 0xff }, /* Blue */
+//    { 0xff, 0xff, 0x00 }, /* Yellow */
+//    { 0x00, 0xff, 0xff }, /* Cyan */
+//    { 0xff, 0x00, 0xff }, /* Magenta */
+//    { 0x80, 0x80, 0x80 }, /* Gray */
+//    { 0x00, 0x00, 0x00 }, /* Black */
+//    { 0xff, 0xff, 0xff }  /* White */
+//  };
+//
+//# define COLORS 9
+//
+//  printf("Audio record program started\n");
+//
+//  if ( (vga_ball_fd = open(filename, O_RDWR)) == -1) {
+//    fprintf(stderr, "could not open %s\n", filename);
+//    return -1;
+//  }
+//
+//  printf("initial state: ");
+//  print_background_color();
+//
+//  for (i = 0 ; i < 24 ; i++) {
+//    set_background_color(&colors[i % COLORS ]);
+//    print_background_color();
+//    usleep(400000);
+//  }
+  
+  printf("Audio record program terminating\n");
+  return 0;
+}
---
+++ write_wav.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include "write_wav.h"
+struct wav_header {
+	char riff[4];
+	int32_t flength;
+	char wave[4];
+	char fmt[4];
+	int32_t format_size;
+	int16_t format_type;
+	int16_t num_chans;
+	int32_t sample_rate;
+	int32_t byte_per_second;
+	int16_t byte_per_sample;
+	int16_t bits_per_sample;
+	char data[4];
+	int32_t dlength;
+}
+
+void write_wav(char *name, const int sample_rate, const int length, 
+	const int duration_sec, int *buffer) 
+{
+	// const int sample_rate = 48000;
+	// const int length = 24;
+	struct wave_header wavh;
+
+	strncpy(wave.riff, "RIFF", 4);
+	strncpy(wave.wave, "WAVE", 4);
+	strncpy(wave.fmt, "fmt", 4);
+	strncpy(wave.data, "data", 4);
+
+	wavh.format_size = length;	// 24
+	wavh.format_type = 1;
+	wavh.num_chans = 1;
+	wavh.sample_rate = sample_rate; // 48000
+	wavh.bits_per_sample = length; //24
+	wavh.byte_per_sample = wavh.bits_per_sample * wavh.num_chans / 8;
+	wavh.byte_per_second = wavh.sample_rate * wavh.byte_per_sample;
+	
+	// const int duration_sec = 10;
+	const int buffer_size = sample_rate * duration_sec;
+
+	wavh.dlength = buffer_size * wavh.byte_per_sample;
+	wavh.flength = wavh.dlength + 44;
+
+	// int buffer[buffer_size] = {};
+
+	FILE *fd = fopen(name, "w");
+	fwrite(&wavh, 1, sizeof(wavh), fd);
+	fwrite(buffer, 4, buffer_size, fd);
+	
+}
