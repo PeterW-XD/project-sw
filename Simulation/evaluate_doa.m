c6e8d751a60b57e959a3eab2b2b26b009abaf558
matheucampbell <matheu.campbell.us@outlook.com>
2024-04-07 16:02:10 -0400

    new sim scripts and results

---
+++ Simulation/evaluate_doa.m
@@ -0,0 +1,45 @@
+function [doa_res, pmax] = evaluate_doa(input_signal, m, d, f, snum)
+    % Array Parameters
+    % m: number of microphones
+    % d: distance between microphones (in meters)
+    
+    % Signal Parameters
+    % f: frequency of signal of interest
+    % snum: number of sectors to split half-circle into
+
+    % Trial Parameters
+    n = length(input_signal);  % samples in data block
+    bnum = snum+1;  % number of beams to form
+
+    % Generate FFT for each microphone
+    L = 1024;
+    spatial_spectrum = zeros(m, L);
+
+    for i=1:m
+        spatial_spectrum(i, :) = fft(input_signal(i, :), L);
+    end
+
+    % Extract bin with FFT peak
+    [~, tbin] = max(spatial_spectrum(1, 1:L/2));
+    tbin = round(tbin);  % Round to nearest integer
+
+    % Compute delay matrix
+    dmat = zeros(m, bnum);  % Rows are sensors, columns are directions
+    angs = (-pi/2:pi/snum:pi/2);
+    wlength = 343/f;
+
+    for n = 1:m  % Iterate over sensors
+        shift_constant = (2*pi*d*(n-1))/wlength;
+        shvec = shift_constant*sin(angs);
+        eshifts = exp(-1i*shvec);
+        dmat(n, :) = eshifts./abs(eshifts);
+    end
+
+    % Apply delays and calculate power
+    sigvec = spatial_spectrum(:, tbin);
+    outvec = dmat.'*sigvec;
+    pwrvec = abs(outvec).^2;
+
+    [pmax, ind] = max(pwrvec);
+    doa_res = angs(ind)*180/pi;
+end
---
+++ Simulation/multiangletest.m
@@ -0,0 +1,36 @@
+% Array Parameters
+m = 8; % number of microphones
+d = 0.10;  % distance between microphones (in meters)
+Fs = 48000;  % sampling rate of the microphones
+
+% Signal Parameters
+f = 15000;  % frequency of signal of interest
+snr = -17;  % signal power to noise power ratio in dBW
+
+% Trial Parameters
+n = 1024;  % samples in data block
+snum = 12; % number of sectors to split half-circle into
+bnum = snum+1;  % number of beams to form
+
+angstep = 5;
+angs = (-90:angstep:90);
+res = zeros(2, length(angs));
+
+for a = 1:length(angs)
+    arrsig = generate_array_signals(m, d, angs(a), f, n, Fs, snr);
+    
+    [resang, respwr] = evaluate_doa(arrsig, m, d, f, snum);
+    res(:, a) = [resang; respwr];
+end
+
+figure(1);
+stem(angs, res(1, :))
+title("Actual vs. Calculated Angle at SNR = 20 dB");
+xlabel("Actual AOA");
+ylabel("Calculated AOA");
+
+figure(2);
+stem(angs, res(2, :))
+title("Actual AOA vs. Max Power");
+xlabel("Actual AOA");
+ylabel("Max Power");
