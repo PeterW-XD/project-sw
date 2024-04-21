219e7a67dded25bdd2412dd08b6d362b5e5eae09
PeterW-XD <peiran_wang@outlook.com>
2024-04-21 16:26:37 -0400

    apr12-test2

--- hello.c
+++ hello.c
@@ -21,7 +21,7 @@
 #define word_length 24		// 24 bits per sample
 #define duration_sec 1		// 1s
 // #define BUF_SIZE (SAMPLE_RATE * duration_sec)
-#define BUF_SIZE 4096
+#define BUF_SIZE 2048
 
 // int vga_ball_fd;
 int audio_fd;
