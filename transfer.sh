4ed8032e06f36eb5ddc8ed503b21d67a376fd639
PeterW-XD <peiran_wang@outlook.com>
2024-05-14 21:25:04 -0400

    changeserver

--- transfer.sh
+++ transfer.sh
@@ -1,3 +1,3 @@
 # scp ./test1.wav ./data.txt pw2593@micro27.ee.columbia.edu:~/4840-Embedded
 # scp ./data1.txt ./data2.txt ./data3.txt ./data4.txt pw2593@micro22.ee.columbia.edu:~/4840-Embedded
-scp ./mic1_real.txt ./mic1_imag.txt ./mic2_real.txt ./mic2_imag.txt ./mic3_real.txt ./mic3_imag.txt ./mic4_real.txt ./mic4_imag.txt pw2593@micro20.ee.columbia.edu:~/4840-Embedded/a_fft
+scp ./mic1_real.txt ./mic1_imag.txt ./mic2_real.txt ./mic2_imag.txt ./mic3_real.txt ./mic3_imag.txt ./mic4_real.txt ./mic4_imag.txt pw2593@micro18.ee.columbia.edu:~/4840-Embedded/a_fft
